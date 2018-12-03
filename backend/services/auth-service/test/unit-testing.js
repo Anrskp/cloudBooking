257b38ca2e1e2b3948a24b3a8141d28da7c6b5e7
Anders <anrskp@gmail.com>
2018-12-03 11:16:42 +0100

    added user tests

---
+++ backend/services/auth-service/test/unit-testing.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const User = require('../models/user');
+const chai = require('chai');
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+
+describe('Unit testing the user model', () => {
+
+// Moc});k user
+var testUser = new User({
+  companyID: "1",
+  username: "mockUser",
+  password: "password",
+  email: "a@valid.email",
+  name: "Test User"
+});
+
+// Create new user
+it('Should add a new user with a hashed password to DB' , (done) => {
+  User.addUser(testUser, (err, user) => {
+    if(err) console.log(err);
+    else {
+      assert.typeOf(user, 'Object');
+      assert.equal(user.username, "mockUser");
+      expect(user.password).to.not.equal("password");
+    }
+    done();
+  });
+});
+
+// Find user
+it('Should find a user with username "mockUser" from DB' , (done) => {
+  User.getUserByUsername(testUser.username, (err, user) => {
+    if (err) console.log(err);
+    else {
+      assert.typeOf(user, 'Object');
+      assert.equal(user.username, testUser.username);
+      assert.equal(user.email, testUser.email);
+      hashedPassword = user.password;
+    }
+    done();
+  })
+});
+
+// Compare password
+
+  it('Should compare passwords and return true' , (done) => {
+    User.comparePassword("password", hashedPassword, (err, isMatch) => {
+      if (err) console.log(err);
+      else {
+        assert.equal(isMatch, true);
+      }
+      done();
+    })
+  });
+
+
+  it('Should compare passwords and return false' , (done) => {
+    User.comparePassword("invalidPassword", hashedPassword, (err, isMatch) => {
+      if (err) console.log(err);
+      else {
+        assert.equal(isMatch, false);
+      }
+      done();
+    })
+  });
+
+
+// Delete user
+it('Should delete a user with username "mockUser"' , (done) => {
+  User.deleteUserByUsername('mockUser', (err, response) => {
+    if(err) console.log(err);
+    else {
+      let amountDeleted = response.result.n
+      expect(amountDeleted).to.be.at.least(1);
+    }
+    done();
+  });
+});
+
+});
