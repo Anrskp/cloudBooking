340ef5fcfd028d7282ad700f95eca77540406f49
Anders <anrskp@gmail.com>
2018-12-15 00:19:26 +0100

    qf

--- backend/services/booking-service/controller/booking-controller.js
+++ backend/services/booking-service/controller/booking-controller.js
@@ -42,71 +42,145 @@ function createBooking(req, res) {
       success: true,
       msg: 'booking added successfully'
     });
-  })
+  });
 
-}
+  // get bookings by user ID
+  function getBookings(req, res) {
 
-// get bookings by user ID
-function getBookings(req, res) {
+    // get bookings invited to
+    let userID = req.params.id;
 
-  // get bookings invited to
-  let userID = req.params.id;
+    let allBookings = [];
 
-  let allBookings = [];
+    const inviteQuery = {
+      invites: userID
+    };
 
-  const inviteQuery = {
-    invites: userID
-  };
+    Booking.find(inviteQuery, (err, bookings) => {
+      if (err) return console.log(err);
+      if (bookings.length) {
+        allBookings.push(bookings);
+      }
+    })
 
-  Booking.find(inviteQuery, (err, bookings) => {
-    if (err) return console.log(err);
-    if (bookings.length) {
+    // get bookings created
+    const query = {
+      userID: userID
+    };
+
+    Booking.find(query, (err, bookings) => {
+      if (err) {
+        res.status('505');
+        return res.json({
+          success: false,
+          msg: 'failed to retrieve bookings'
+        });
+      }
+
+      if (!bookings.length) {
+        res.status('505');
+        return res.json({
+          success: false,
+          msg: "Could not find any bookings for userID " + userID
+        })
+      }
+
       allBookings.push(bookings);
-    }
-  })
 
-  // get bookings created
-  const query = {
-    userID: userID
-  };
+      res.status('505');
+      return res.json({
+        success: true,
+        allBookings
+      });
+    });
+  }
 
-  Booking.find(query, (err, bookings) => {
-    if (err) {
+  // update booking by bookingID
+  function editBooking(req, res) {
+
+    // check parameters
+    req.checkBody('userID').notEmpty();
+    req.checkBody('title').notEmpty();
+    req.checkBody('start').notEmpty();
+    req.checkBody('end').notEmpty();
+
+    let errors = req.validationErrors();
+
+    if (errors) {
       res.status('505');
       return res.json({
         success: false,
-        msg: 'failed to retrieve bookings'
+        errors
       });
     }
 
-    if (!bookings.length) {
-      res.status('505');
-      return res.json({
-        success: false,
-        msg: "Could not find any bookings for userID " + userID
-      })
+    let bookingID = req.params.id;
+
+    const query = {
+      _id: bookingID
     }
 
-    allBookings.push(bookings);
+    Booking.updateOne(query, req.body, {
+      new: true
+    }, (err, booking) => {
+      if (err) {
+        res.status('505')
+        return res.json({
+          success: false,
+          msg: 'failed to update booking with id \'' + bookingID + '\''
+        });
+      }
 
-    res.status('505');
-    return res.json({
-      success: true,
-      allBookings
+      res.status('505');
+      return res.json({
+        success: true,
+        msg: 'updated record with id  \'' + bookingID + '\' successfully'
+      });
     });
-  });
-}
+  };
 
-// update booking by bookingID
-function editBooking(req, res) {
+  // delete booking by bookingID
+  function deleteBooking(req, res) {
+    let bookingID = req.params.id;
+    const query = {
+      _id: bookingID
+    }
+
+    Booking.remove(query, (err, result) => {
+      if (err) {
+        res.status('505');
+        return res.json({
+          success: false,
+          msg: 'failed to delete booking with id \'' + bookingID + '\''
+        });
+      }
+
+      res.status('505');
+      return res.json({
+        success: true,
+        msg: 'deleted record with id \'' + bookingID + '\''
+      });
+    })
+  }
+
+  // exports api functions
+  module.exports = {
+    getBookings,
+    createBooking,
+    editBooking,
+    deleteBooking
+  };
+const mongoose = require('mongoose');
+const Booking = require('../models/booking');
+
+// add new bookin
+function createBooking(req, res) {
 
   // check parameters
   req.checkBody('userID').notEmpty();
   req.checkBody('title').notEmpty();
   req.checkBody('start').notEmpty();
   req.checkBody('end').notEmpty();
-
-  // sanitize
 
   let errors = req.validationErrors();
 
@@ -118,59 +192,153 @@ function editBooking(req, res) {
     });
   }
 
-  let bookingID = req.params.id;
-
-  const query = {
-    _id: bookingID
-  }
-
-  Booking.updateOne(query, req.body, {
-    new: true
-  }, (err, booking) => {
-    if (err) {
-      res.status('505')
-      return res.json({
-        success: false,
-        msg: 'failed to update booking with id \'' + bookingID + '\''
-      });
-    }
-
-    res.status('505');
-    return res.json({
-      success: true,
-      msg: 'updated record with id  \'' + bookingID + '\' successfully'
-    });
+  let newBooking = new Booking({
+    userID: req.body.userID,
+    title: req.body.title,
+    start: req.body.start,
+    end: req.body.end,
+    invites: req.body.invites
   });
-};
 
-// delete booking by bookingID
-function deleteBooking(req, res) {
-  let bookingID = req.params.id;
-  const query = {
-    _id: bookingID
-  }
-
-  Booking.remove(query, (err, result) => {
+  newBooking.save((err, booking) => {
     if (err) {
       res.status('505');
       return res.json({
         success: false,
-        msg: 'failed to delete booking with id \'' + bookingID + '\''
+        msg: 'failed to add new booking'
       });
     }
 
     res.status('505');
     return res.json({
       success: true,
-      msg: 'deleted record with id \'' + bookingID + '\''
+      msg: 'booking added successfully'
     });
-  })
-}
+  });
 
-// exports api functions
-module.exports = {
-  getBookings,
-  createBooking,
-  editBooking,
-  deleteBooking
-};
+  // get bookings by user ID
+  function getBookings(req, res) {
+
+    // get bookings invited to
+    let userID = req.params.id;
+
+    let allBookings = [];
+
+    const inviteQuery = {
+      invites: userID
+    };
+
+    Booking.find(inviteQuery, (err, bookings) => {
+      if (err) return console.log(err);
+      if (bookings.length) {
+        allBookings.push(bookings);
+      }
+    })
+
+    // get bookings created
+    const query = {
+      userID: userID
+    };
+
+    Booking.find(query, (err, bookings) => {
+      if (err) {
+        res.status('505');
+        return res.json({
+          success: false,
+          msg: 'failed to retrieve bookings'
+        });
+      }
+
+      if (!bookings.length) {
+        res.status('505');
+        return res.json({
+          success: false,
+          msg: "Could not find any bookings for userID " + userID
+        })
+      }
+
+      allBookings.push(bookings);
+
+      res.status('505');
+      return res.json({
+        success: true,
+        allBookings
+      });
+    });
+  }
+
+  // update booking by bookingID
+  function editBooking(req, res) {
+
+    // check parameters
+    req.checkBody('userID').notEmpty();
+    req.checkBody('title').notEmpty();
+    req.checkBody('start').notEmpty();
+    req.checkBody('end').notEmpty();
+
+    let errors = req.validationErrors();
+
+    if (errors) {
+      res.status('505');
+      return res.json({
+        success: false,
+        errors
+      });
+    }
+
+    let bookingID = req.params.id;
+
+    const query = {
+      _id: bookingID
+    }
+
+    Booking.updateOne(query, req.body, {
+      new: true
+    }, (err, booking) => {
+      if (err) {
+        res.status('505')
+        return res.json({
+          success: false,
+          msg: 'failed to update booking with id \'' + bookingID + '\''
+        });
+      }
+
+      res.status('505');
+      return res.json({
+        success: true,
+        msg: 'updated record with id  \'' + bookingID + '\' successfully'
+      });
+    });
+  };
+
+  // delete booking by bookingID
+  function deleteBooking(req, res) {
+    let bookingID = req.params.id;
+    const query = {
+      _id: bookingID
+    };
+
+    Booking.remove(query, (err, result) => {
+      if (err) {
+        res.status('505');
+        return res.json({
+          success: false,
+          msg: 'failed to delete booking with id \'' + bookingID + '\''
+        });
+      };
+
+      res.status('505');
+      return res.json({
+        success: true,
+        msg: 'deleted record with id \'' + bookingID + '\''
+      });
+    });
+  };
+
+  // exports api functions
+  module.exports = {
+    getBookings,
+    createBooking,
+    editBooking,
+    deleteBooking
+  };
