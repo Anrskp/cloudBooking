236977c5d2c6c7b3242d85c52a6913876f794e19
Anders <anrskp@gmail.com>
2018-12-16 13:07:32 +0100

    qf

--- backend/services/auth-service/controller/user-controller.js
+++ backend/services/auth-service/controller/user-controller.js
@@ -15,6 +15,23 @@ function registerUser(req, res) {
   });
 
   // check if email is taken
+  const query = {
+    email: email
+  };
+
+  User.findOne(query, (err, user) => {
+    if (err) {
+      return res.json({
+        success: false,
+        msg: 'failed to register user'
+      });
+    }
+
+    if (user) {
+      return res.json({
+        success: false,
+        msg: 'Email already in use'
+      });
 
   // hash password
   bcrypt.genSalt(10, (err, salt) => {
@@ -97,53 +114,6 @@ function authenticateUser(req, res) {
     });
   })
 }
-/*
-module.exports.comparePassword = function(candidatePassword, hash, callback) {
-  bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
-    if(err) throw err
-    callback(null, isMatch);
-  });
-};
-*/
-
-/*
-User.getUserByEmail(email, (err, user) => {
-  if (err) throw err;
-  if (!user) {
-    return res.json({
-      success: false,
-      msg: 'User not found'
-    });
-  }
-
-  User.comparePassword(password, user.password, (err, isMatch) => {
-    if (err) throw err;
-    if (isMatch) {
-      const token = jwt.sign({
-        data: user
-      }, config.secret, {
-        expiresIn: 7200 // 2 hours
-      });
-
-      res.json({
-        success: true,
-        token: 'JWT ' + token,
-        user: {
-          id: user._id,
-          companyID: user.companyID,
-          email: user.email,
-          username: user.username
-        }
-      });
-    } else {
-      return res.json({
-        success: false,
-        msg: 'Wrong password'
-      });
-    }
-  });
-});
-*/
 
 // Get users by companyID
 function getUsersByCompanyID(req, res) {
@@ -169,103 +139,3 @@ module.exports = {
   editUser,
   deleteUser
 };
-
-/*
-const express = require('express');
-const router = express.Router();
-const passport = require('passport');
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
-const config = require('../config/database');
-
-// Register a new user.
-router.post('/register', (req, res, next) => {
-
-  let newUser = new User({
-    companyID: req.body.companyID,
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  });
-
-  // is email already in use.
-  User.getUserByEmail(newUser.email, (err, user) => {
-    if (err) throw err
-    if (user != null) {
-      res.json({
-        success: false,
-        msg: 'email already in use'
-      })
-    } else {
-      // add user to db
-      User.addUser(newUser, (err, user) => {
-        if (err) res.json({
-          success: false,
-          msg: 'failed to register user'
-        })
-        else res.json({
-          success: true,
-          msg: 'user registered successfully'
-        })
-      })
-    }
-  })
-});
-
-
-// Authenticate a user
-router.post('/authenticate', (req, res, next) => {
-
-  const password = req.body.password;
-  const email = req.body.email;
-
-  User.getUserByEmail(email, (err, user) => {
-    if (err) throw err;
-    if (!user) {
-      return res.json({
-        success: false,
-        msg: 'User not found'
-      });
-    }
-
-    User.comparePassword(password, user.password, (err, isMatch) => {
-      if (err) throw err;
-      if (isMatch) {
-        const token = jwt.sign({
-          data: user
-        }, config.secret, {
-          expiresIn: 7200 // 2 hours
-        });
-
-        res.json({
-          success: true,
-          token: 'JWT ' + token,
-          user: {
-            id: user._id,
-            email: user.email,
-            username: user.username
-          }
-        });
-      } else {
-        return res.json({
-          success: false,
-          msg: 'Wrong password'
-        });
-      }
-    });
-  });
-});
-
-router.post('/getEmployees', (req, res, next) => {
-
-  const companyID = req.body.companyID;
-
-  User.getUsersByCompanyID(companyID, (err, response) => {
-    if (err) res.json({success: false, msg: "failed to get users"});
-    else if(response.length == 0) res.json({success: false, msg: 'no employees with that companyID'});
-    else res.json({success: true, response});
-  });
-})
-
-module.exports.router = router;
-*/
