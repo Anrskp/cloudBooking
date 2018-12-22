5308181ff8b027b05dbfc60b22d2181846b3a179
Anders <anrskp@gmail.com>
2018-12-22 16:47:49 +0100

    spelling error fix

--- backend/api-gateway/routes/booking-route.js
+++ backend/api-gateway/routes/booking-route.js
@@ -12,10 +12,10 @@ module.exports = function(app) {
   app.route('/booking/entity/:id')
     .get(bookingCtrl.getEntityBookings);
 
-  app.route('/booking/entityAvalability/:id/:start/:end')
+  app.route('/booking/entityAvailability/:id/:start/:end')
     .get(bookingCtrl.checkEntityAvailability);
 
-  app.route('/booking/userAvalability/:id/:start/:end')
+  app.route('/booking/userAvailability/:id/:start/:end')
     .get(bookingCtrl.checkAvailability);
 
 };
